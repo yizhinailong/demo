58a776e4a6252956959e73bf03d591b00de44b1d
yizhinailong <liufeng.code@outlook.com>
2026-01-13 02:40:01 +0800

    refactor(cmd_parser): simplify command line argument declarations
    - Consolidate multi-line argument declarations into single lines where possible
    - Maintain proper formatting for long argument definitions that exceed line limits
    - Remove unnecessary line breaks and indentation in argument definitions

--- cmd_parser/main.cpp
+++ cmd_parser/main.cpp
@@ -4,27 +4,16 @@
 
 int main(int argc, char* argv[]) {
     cmdline::parser args;
-    args.add<std::string>("host",
-                          'h',
-                          "host name",
-                          true,
-                          "");
-    args.add<int>("port",
-                  'p',
-                  "port number",
-                  false,
-                  80,
-                  cmdline::range(1, 65535));
-    args.add<std::string>("type",
-                          't',
-                          "protocol type",
-                          false,
-                          "http",
-                          cmdline::oneof<std::string>("http", "https", "ssh", "ftp"));
-    args.add("gzip",
-             '\0',
-             "gzip when transfer");
-
+    args.add<std::string>("host", 'h', "host name", true, "");
+    args.add<int>("port", 'p', "port number", false, 80, cmdline::range(1, 65535));
+    args.add<std::string>(
+        "type",
+        't',
+        "protocol type",
+        false,
+        "http",
+        cmdline::oneof<std::string>("http", "https", "ssh", "ftp"));
+    args.add("gzip", '\0', "gzip when transfer");
     args.parse_check(argc, argv);
 
     std::cout << args.get<std::string>("type") << "://"
