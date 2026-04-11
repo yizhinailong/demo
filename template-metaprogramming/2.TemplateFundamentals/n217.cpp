b7944e46b1aadedd6736edc208acd5c0e3037700
yizhinailong <liufeng.code@outlook.com>
2026-04-11 17:09:39 +0800

    feat(template-metaprogramming): add template fundamentals example with default parameters
    Add a new example file demonstrating template fundamentals with default template parameters,
    including two template classes (foo and bar) with different parameter configurations.

---
+++ template-metaprogramming/2.TemplateFundamentals/n217.cpp
@@ -0,0 +1,14 @@
+import std;
+
+namespace n217 {
+    template <typename T = int>
+    class foo {};
+
+    template <typename T = int, typename U = double>
+    class bar {};
+
+} // namespace n217
+
+auto main() -> int {
+    return 0;
+}
