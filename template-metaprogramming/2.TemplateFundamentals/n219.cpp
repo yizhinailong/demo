6635e05c80e51ad51f49d02ea1fc28f76b7046e3
yizhinailong <liufeng.code@outlook.com>
2026-04-11 17:19:20 +0800

    feat(template-metaprogramming): implement template struct with default parameters
    - Add template struct 'foo' with default type parameter U as double
    - Change default type parameter T from no default to int
    - Define struct members a of type T and b of type U
    - Add example usage in main function demonstrating template instantiation

--- template-metaprogramming/2.TemplateFundamentals/n219.cpp
+++ template-metaprogramming/2.TemplateFundamentals/n219.cpp
@@ -1,10 +1,24 @@
 import std;
 
 namespace n219 {
+    template <typename T, typename U = double>
+    struct foo;
 
-}
+    template <typename T = int, typename U>
+    struct foo;
+
+    template <typename T, typename U>
+    struct foo {
+        T a;
+        U b;
+    };
+} // namespace n219
 
 auto main() -> int {
-    {}
+    {
+        using namespace n219;
+
+        foo f{ 10, 10.0 };
+    }
     return 0;
 }
