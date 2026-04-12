2a51cacfd09bc00d6511d356f9963c45c810e2cc
yizhinailong <liufeng.code@outlook.com>
2026-04-12 18:34:49 +0800

    feat(template-metaprogramming): add template fundamentals example with partial specialization
    Add a new C++ template metaprogramming example demonstrating partial template
    specialization techniques with integer template parameters.
    
    The example shows three template specializations:
    - Generic template for two different integer parameters
    - Specialized template when both parameters are equal
    - Specialized template when second parameter is one greater than first

---
+++ template-metaprogramming/2.TemplateFundamentals/n236.cpp
@@ -0,0 +1,16 @@
+import std;
+
+namespace n236 {
+    template <int A, int B>
+    struct foo {};
+
+    template <int A>
+    struct foo<A, A> {};
+
+    template <int A>
+    struct foo<A, A + 1> {};
+} // namespace n236
+
+auto main() -> int {
+    return 0;
+}
