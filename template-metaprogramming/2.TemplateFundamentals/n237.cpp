bac7396287a1db56b7fb94b330ec2b6715de4f95
yizhinailong <liufeng.code@outlook.com>
2026-04-12 18:50:29 +0800

    feat(template-metaprogramming): add template fundamentals example with array pretty printer
    Add a new example demonstrating template fundamentals featuring:
    - A generic pretty_print function for std::array with type and size parameters
    - Implementation showing array iteration and formatted output
    - Test cases with both integer and character arrays
    - Usage of std::ranges::copy for string to array conversion

---
+++ template-metaprogramming/2.TemplateFundamentals/n237.cpp
@@ -0,0 +1,31 @@
+import std;
+
+namespace n237 {
+    template <typename T, int S>
+    auto pretty_print(std::ostream& os, std::array<T, S> const& arr) -> std::ostream& {
+        os << '[';
+        if (S > 0) {
+            for (std::size_t i = 0; i < S - 1; ++i) {
+                os << arr[i] << ", ";
+            }
+            os << arr[S - 1];
+        }
+        os << "]\n";
+        return os;
+    }
+} // namespace n237
+
+auto main() -> int {
+    {
+        using namespace n237;
+
+        std::array<int, 8> arr{ 1, 2, 3, 4, 5, 6, 7, 8 };
+        pretty_print<int, 8>(std::cout, arr);
+
+        std::array<char, 8>        arr_char{};
+        constexpr std::string_view sv = "template";
+        std::ranges::copy(sv, arr_char.begin());
+        pretty_print<char, 8>(std::cout, arr_char);
+    }
+    return 0;
+}
