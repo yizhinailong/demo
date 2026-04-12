4e4f34ae77510a41c33032d81dbf96a3337a47b5
yizhinailong <liufeng.code@outlook.com>
2026-04-12 21:10:56 +0800

    refactor(template-metaprogramming): update template parameter type and simplify function calls
    - change template parameter from 'int S' to 'std::size_t S' for better type safety
    - remove explicit template arguments in function calls as they can be deduced automatically
    - improve code readability by using argument-dependent lookup for template parameter deduction

--- template-metaprogramming/2.TemplateFundamentals/n237.cpp
+++ template-metaprogramming/2.TemplateFundamentals/n237.cpp
@@ -1,7 +1,7 @@
 import std;
 
 namespace n237 {
-    template <typename T, int S>
+    template <typename T, std::size_t S>
     auto pretty_print(std::ostream& os, std::array<T, S> const& arr) -> std::ostream& {
         os << '[';
         if (S > 0) {
@@ -20,12 +20,12 @@ auto main() -> int {
         using namespace n237;
 
         std::array<int, 8> arr{ 1, 2, 3, 4, 5, 6, 7, 8 };
-        pretty_print<int, 8>(std::cout, arr);
+        pretty_print(std::cout, arr);
 
         std::array<char, 8>        arr_char{};
         constexpr std::string_view sv = "template";
         std::ranges::copy(sv, arr_char.begin());
-        pretty_print<char, 8>(std::cout, arr_char);
+        pretty_print(std::cout, arr_char);
     }
     return 0;
 }
