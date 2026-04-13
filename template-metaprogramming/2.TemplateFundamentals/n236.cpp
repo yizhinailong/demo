729986bb42c5f573671539dd4dddc3de3f83e177
yizhinailong <liufeng.code@outlook.com>
2026-04-13 09:10:15 +0800

    refactor(template-metaprogramming): comment out redundant template specialization
    - Commented out the template specialization `foo<A, A + 1>` that was
      causing conflicts with the primary template and other specializations
    - This resolves potential compilation issues in the template fundamentals
      example

--- template-metaprogramming/2.TemplateFundamentals/n236.cpp
+++ template-metaprogramming/2.TemplateFundamentals/n236.cpp
@@ -7,8 +7,8 @@ namespace n236 {
     template <int A>
     struct foo<A, A> {};
 
-    template <int A>
-    struct foo<A, A + 1> {};
+    // template <int A>
+    // struct foo<A, A + 1> {};
 } // namespace n236
 
 auto main() -> int {
